324a1b87416d304f4c79d7012f0bdcf7b4c3727c
Michael Freese <mrfreese1@gmail.com>
2016-11-30 16:14:25 -0500

    changestocontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   # respond_to :json
-  acts_as_token_authentication_handler_for User
+
+  # acts_as_token_authentication_handler_for User
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  acts_as_token_authenticatable
+
+
+
 end
