37c90ef7d065774e4108f77f9235d88d46e332cc
Troy Denney <tvdenney@gmail.com>
2016-12-05 16:53:30 -0500

    added logo and alternate page

--- webpack/components/Criteria.js
+++ webpack/components/Criteria.js
@@ -27,24 +27,41 @@ class Criteria extends React.Component {
     //
     // }
 
+    // $(document).ready(function(){
+    //
+    //   $("input[type='range']").change(function() {
+    //     slider = $(this);
+    //     value = (slider.val() -1);
+    //
+    //     $('p.rangeLabel').removeClass('selected');
+    //     $('p.rangeLabel:eq(' + value + ')').addClass('selected');
+    //
+    //   });
+    //
+    //   $('p.rangeLabel').bind('click', function(){
+    //     label = $(this);
+    //     value = label.index();
+    //     $("input[type='range']").attr('value', value)
+    //                             .trigger('change');
+    //   });
+    //
+    // });
+
     render() {
         return <div>
             <div className="row critTopGrad">
                 {/* <!-- Top picture --> */}
                 <div className="col-sm-9 critImage">
                     <div className="critWhiteAndShadow">
-                        <img src="../img/logoTest2.svg" alt="Logo" className="suLogo center-block"/>
-                        <h1>Restaurant Randomizer</h1>
+                        {/* <img src="../img/logoTest2.svg" alt="Logo" className="suLogo center-block"/> */}
+                        <img src="../img/logo3.svg" alt="Logo" className="suLogo1"/>
+                        {/* <h1>Restaurant Randomizer</h1> */}
                     </div>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
+
                     <div className="critWhiteAndShadow">
-                        <h3>Here is where the rules will be placed.</h3>
+                        <h4>Choose type, price and distance and press enter.</h4>
+                        <h4>You'll receive an email giving you a random selection.</h4>
+                        <h4>You can reject the selection twice, but 3rd time is the charm.</h4>
                     </div>
                 </div>
                 <div className="col-sm-3 critCenter">
@@ -83,6 +100,13 @@ class Criteria extends React.Component {
                     <div className="form-group critCenter">
                        <label htmlFor="cost">Price</label>
                        <input type="range" min="1" max="4" defaultValue="1" className="form-control" id="cost" />
+                       {/* <div class="rangeWrapper">
+                            <input id="slide" type="range" min="1" max="4" step="1" value="1"  />
+                            <p class="rangeLabel selected">Label A</p>
+                            <p class="rangeLabel">Label B</p>
+                            <p class="rangeLabel">Label C</p>
+                            <p class="rangeLabel">Label D</p>
+                        </div> */}
                      </div>
                 </div>
                 <div className="col-sm-4">
