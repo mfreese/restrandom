91a1253d19a20e5feffd24f9a06a5da6d9510dd9
Troy Denney <tvdenney@gmail.com>
2016-12-14 15:53:12 -0500

    starting user update

--- webpack/components/Header.js
+++ webpack/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Header = (props) => <div className="row">
-    <div className="col-sm-10 topBar fontColorShadowCrit">
+    <div className="col-sm-8 topBar fontColorShadowCrit">
             <a className="fontHead fontColorShadowCrit" href="/criteria">Restaurant Randomizer</a>
             {/* <h1 className="fontHead">Restaurant Randomizer</h1> */}
+    </div>
+    <div className="col-sm-2 topBar">
+        <form className="button_to" method="post" action="/users/sign_out"><input type="hidden" name="_method" value="delete" /><input className="btn btn-md btn-info loButton loMargin" type="submit" value="Update!" /></form>
     </div>
     <div className="col-sm-2 topBar">
         <form className="button_to" method="post" action="/users/sign_out"><input type="hidden" name="_method" value="delete" /><input className="btn btn-md btn-danger loButton loMargin" type="submit" value="Log Out!" /></form>
